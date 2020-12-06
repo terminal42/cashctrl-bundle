a3a5eb8c6b0e9af006e601471f874be6b330fcf3
Andreas Schempp <andreas.schempp@terminal42.ch>
2020-12-06 22:19:37 +0100

    Added automatic throttling of API requests

--- src/DependencyInjection/Terminal42CashctrlExtension.php
+++ src/DependencyInjection/Terminal42CashctrlExtension.php
@@ -4,9 +4,9 @@ namespace Terminal42\CashctrlBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Terminal42\CashctrlApi\ApiClient;
 
 class Terminal42CashctrlExtension extends ConfigurableExtension
 {
@@ -24,6 +24,10 @@ class Terminal42CashctrlExtension extends ConfigurableExtension
         );
 
         $loader->load('services.yml');
+
+        if ($mergedConfig['throttle']) {
+            $container->getDefinition(ApiClient::class)->addMethodCall('enableAutoThrottle');
+        }
 
         $container->setParameter('terminal42_cashctrl.subdomain', $mergedConfig['subdomain']);
         $container->setParameter('terminal42_cashctrl.api_key', $mergedConfig['api_key']);
